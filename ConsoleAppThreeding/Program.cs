3cd8a81528a5782affe30b475cc223aaa005e91b
Mari <uncharted220501@hotmail.com>
2020-12-14 14:14:15 +0100

    Add project files.

---
+++ ConsoleAppThreeding/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleAppThreeding
+{
+    class Program
+    {
+        static char ch = '*';
+        static void Main(string[] args)
+        {
+            Threprog threprog = new Threprog();
+            #region opg 0,1
+            //Thread thread = new Thread(new ThreadStart(threprog.WorkThreadFuntion));
+            //thread.Start()
+            #endregion
+            #region Opg 2
+            //Thread thread1 = new Thread(threprog.WorkThreadFuntion);
+            //thread1.Start("C#-trådning er nemt!");
+
+            //Thread.Sleep(1000);
+
+            //Thread thread2 = new Thread(threprog.WorkThreadFuntion);
+            //thread2.Start(" Også med flere tråde …");
+            #endregion
+            #region opg 3
+            //Thread thread3 = new Thread(threprog.ErrorMge);
+            //thread3.Start();
+
+            //Thread.Sleep(10000); // main tread sleap
+            //bool isStilAlive = thread3.IsAlive;
+
+            //if (isStilAlive == false)
+            //{
+            //    thread3.Abort();
+            //}
+            #endregion
+
+
+
+            Thread thread4 = new Thread(PrintUsrInput);
+            thread4.Start();
+
+            Thread thread5 = new Thread(UsrInput);
+            thread5.Start();
+
+            Console.Read();
+
+            Console.ReadKey();
+        }
+
+        static public void PrintUsrInput()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                Console.Write(ch);
+            }
+        }
+
+        static public void UsrInput()
+        {
+            while (true)
+            {
+                ch = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
---
+++ ConsoleAppThreeding/Threprog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleAppThreeding
+{
+    public class Threprog
+    {
+        public void WorkThreadFuntion(object txt)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(txt);
+            }
+        }
+
+        private int RanNo()
+        {
+            Random Random = new Random();
+            return Random.Next(-20, 121);
+        }
+
+        public void ErrorMge()
+        {
+            int breakPull = 0;
+            while (breakPull < 3)
+            {
+                int temp = Convert.ToInt32(RanNo());
+                if (temp > 100 || temp < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("error magesse: can´t not show those temperatures ({0})", temp);
+                    Console.ResetColor();
+                    breakPull++;
+                }
+                else
+                    Console.WriteLine("temperatur is {0}", temp);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
